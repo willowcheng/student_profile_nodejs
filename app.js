6ce28f14dbd34397f56f66a32e7007cec925a3f3
willowcheng <e.wingus@Gmail.com>
2015-11-02 13:01:01 -0500

    Create simple server

--- app.js
+++ app.js
@@ -3,6 +3,16 @@
 
 //1. Create a web server
 
+var http = require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    setInterval(function () {
+        response.write(new Date() + "\n");
+    }, 1000);
+    //response.end('Hello World\n');
+}).listen(3000);
+console.log('Server running at http://<workspace-url>/');
+
 //2. Handle HTTP route GET / and POST / i.e. Home
     //if url == "/" && GET
         //show search
