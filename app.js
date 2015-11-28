8ace79c222400c80c4078772506b82f21eb32acf
willowcheng <e.wingus@Gmail.com>
2015-11-28 17:10:09 -0500

    Add home route

--- app.js
+++ app.js
@@ -5,19 +5,25 @@
 
 var http = require('http');
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    setInterval(function () {
-        response.write(new Date() + "\n");
-    }, 1000);
-    //response.end('Hello World\n');
+    homeRoute(request, response);
 }).listen(3000);
 console.log('Server running at http://<workspace-url>/');
 
 //2. Handle HTTP route GET / and POST / i.e. Home
+function homeRoute(request, response) {
     //if url == "/" && GET
+    if(request.url === "/") {
         //show search
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("Header\n");
+        response.write("Search\n");
+        response.end('Footer\n');
+    }
     //if url == "/" && POST
-        //redirect to /:username
+    //redirect to /:username
+}
+
+
 
 //3. Handle HTTP route GET /:username i.e. /chalkers
     //if url == "/...."
